bfb225a1051426bebe0149d99c6ee7d38130275c
Rajveer Singh Chandel <chandelrajveer7896@gmail.com>
2025-08-18 22:45:23 +0530

    set up init,add,commit commands

--- backend/controllers/add.js
+++ backend/controllers/add.js
@@ -1,5 +1,18 @@
-async function addRepo() {
-  console.log("Adding file command is called!!");
+const fs = require("fs").promises;
+const path = require("path");
+
+async function addRepo(filePath) {
+  const repoPath = path.resolve(process.cwd(), ".myGit");
+  const stagingPath = path.join(repoPath, "staging");
+
+  try {
+    await fs.mkdir(stagingPath, { recursive: true });
+    const fileName = path.basename(filePath);
+    await fs.copyFile(filePath, path.join(stagingPath, fileName));
+    console.log(`File ${fileName} is added to the staging area!!`);
+  } catch (err) {
+    console.error("Error in adding file: " + err);
+  }
 }
 
 module.exports = { addRepo };
--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,5 +1,21 @@
+const fs = require("fs").promises;
+const path = require("path");
+
 async function initRepo() {
-    console.log("initialising repository command called!!")
+  const repoPath = path.resolve(process.cwd(), ".myGit");
+  const commitsPath = path.join(repoPath, "commits");
+
+  try {
+    await fs.mkdir(repoPath, { recursive: true });
+    await fs.mkdir(commitsPath, { recursive: true });
+    await fs.writeFile(
+      path.join(repoPath, "config.json"),
+      JSON.stringify({ bucket: process.env.S3_BUCKET })
+    );
+    console.log("Repository Initilised");
+  } catch (err) {
+    console.error("Error in Initialising Repository : " + err);
+  }
 }
 
-module.exports = {initRepo} ;
+module.exports = { initRepo };
