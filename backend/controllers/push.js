b15d0d7dde5d5a5f329a5b5eebec7c252be506d4
Rajveer Singh Chandel <chandelrajveer7896@gmail.com>
2025-08-19 23:27:10 +0530

    aws configuring and worked on push command

--- backend/controllers/push.js
+++ backend/controllers/push.js
@@ -1,5 +1,35 @@
+const fs = require("fs").promises ;
+const path = require("path") ;
+const {s3 , S3_BUCKET} = require("../config/aws-config")
+
 async function pushRepo() {
-  console.log("push commits to s3 command is called!!");
+  const repoPath = path.resolve(process.cwd(),".myGit"); 
+  const commitsPath = path.join(repoPath,"commits");
+
+  try{
+    const commitDirs = await fs.readdir(commitsPath);
+    for(const commitDir of commitDirs){
+      const commitPath = path.join(commitsPath,commitDir);
+
+      const files = await fs.readdir(commitPath);
+      for(const file of files){
+        const filePath = path.join(commitPath,file);
+        const fileContent = await fs.readFile(filePath);
+        
+        const params = {
+          Bucket: S3_BUCKET,
+          Key: `commits/${commitDir}/${file}`,
+          Body: fileContent
+        };
+
+        await s3.upload(params).promise() ;
+      }
+    }
+
+    console.log("All commits pushed to s3") ;
+  }catch(err){
+    console.error("Error in pushing on s3 : " + err) ;
+  }
 }
 
 module.exports = { pushRepo };
