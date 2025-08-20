f9eeabe0de493455052511e906f5d701e34169f0
Rajveer Singh Chandel <chandelrajveer7896@gmail.com>
2025-08-20 20:23:53 +0530

    connected with mongoDB impleamented socket.io and create models

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,11 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
 
@@ -8,7 +16,10 @@ const { pushRepo } = require("./controllers/push");
 const { pullRepo } = require("./controllers/pull");
 const { revertRepo } = require("./controllers/revert");
 
+dotenv.config();
+
 yargs(hideBin(process.argv))
+  .command("start", "Start a new server", {}, startServer)
   .command("init", "Initialise a new repository", {}, initRepo)
   .command(
     "add <file>",
@@ -53,3 +64,53 @@ yargs(hideBin(process.argv))
   )
   .demandCommand(1, "You need to give atleast one command")
   .help().argv;
+
+function startServer() {
+  const app = express();
+  const port = process.env.PORT || 3000;
+
+  app.use(bodyParser.json());
+  app.use(express.json());
+
+  const mongoURI = process.env.MONGODG_URI;
+
+  mongoose
+    .connect(mongoURI)
+    .then(() => console.log("connected to mongoDB"))
+    .catch((err) => console.log("Unable to connect : ", err));
+
+  app.use(cors({ origin: "*" }));
+  app.get("/", (req, res) => {
+    res.send("Welcome!!") ;
+  });
+
+  let user = "test";
+
+  const httpServer = http.createServer(app);
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection",(socket) => {
+    socket.on("joinRoom",(userID) => {
+      user = userID ;
+      console.log("=====") ;
+      console.log(user) ;
+      console.log("=====");
+      socket.join(userID) ;
+    })
+  })
+
+  const db = mongoose.connection ;
+
+  db.once("open",async() => {
+    console.log("CRUD Operations called") ;
+  })
+
+  httpServer.listen(port ,() => {
+    console.log(`server running at port ${port}`) ;
+  })
+}
