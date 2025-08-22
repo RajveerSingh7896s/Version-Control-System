084bf7331ae62e0e57345b87abd51a4db20b45e2
Rajveer Singh Chandel <chandelrajveer7896@gmail.com>
2025-08-22 17:51:48 +0530

    impleamented users all functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ReturnDocument } = require("mongodb");
 const dotenv = require("dotenv");
+const ObjectId = require("mongodb").ObjectId;
 
 dotenv.config();
 
@@ -46,32 +47,133 @@ const signup = async (req, res) => {
       process.env.JWT_SECRET_KEY,
       { expiresIn: "1h" }
     );
-    
-    res.json({token});
+
+    res.json({ token });
   } catch (err) {
     console.error("Error in signing up : ", err);
     res.status(500).send("Server Error!!");
   }
 };
 
-const login = (req, res) => {
-  res.send("USER LOGGING IN...");
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    await connectClient();
+    const db = client.db("version-control-system");
+    const usersCollection = db.collection("users");
+
+    const user = await usersCollection.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Creadential" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Creadential" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1h",
+    });
+
+    res.json({ token, userId: user._id });
+  } catch (err) {
+    console.error("Error during logIn : ", err.message);
+    res.status(500).send("Server Error!");
+  }
 };
 
-const getAllUser = (req, res) => {
-  res.send("ALL USERS FETCHED...");
+const getAllUser = async (req, res) => {
+  try {
+    await connectClient();
+    const db = client.db("version-control-system");
+    const usersCollection = db.collection("users");
+
+    const users = await usersCollection.find({}).toArray();
+    res.json(users);
+  } catch (err) {
+    console.error("Error during logIn : ", err.message);
+    res.status(500).send("Server Error!");
+  }
 };
 
-const getUserProfile = (req, res) => {
-  res.send("GETTING USER PROFILE...");
+const getUserProfile = async (req, res) => {
+  const currentID = req.params.id;
+  try {
+    await connectClient();
+    const db = client.db("version-control-system");
+    const usersCollection = db.collection("users");
+
+    const user = await usersCollection.findOne({
+      _id: new ObjectId(currentID),
+    });
+    if (!user) {
+      return res.status(404).json({ message: "User Not Found!" });
+    }
+
+    res.send(user);
+  } catch (err) {
+    console.error("Error during fetching : ", err.message);
+    res.status(500).send("Server Error!");
+  }
 };
 
-const updateUserProfile = (req, res) => {
-  res.send("UPDATING USER PROFILE....");
+const updateUserProfile = async (req, res) => {
+  const currentID = req.params.id;
+  const { email, password } = req.body;
+
+  await connectClient();
+  const db = client.db("version-control-system");
+  const usersCollection = db.collection("users");
+
+  try {
+    let updateFields = {};
+    if (email) updateFields.email = email;
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      updateFields.password = hashedPassword;
+    }
+
+    const result = await usersCollection.findOneAndUpdate(
+      {
+        _id: new ObjectId(currentID),
+      },
+      { $set: updateFields },
+      { returnDocument: "after" }
+    );
+    if (!result) {
+      return res.status(404).json({ message: "User Not Found!" });
+    }
+
+    res.json({message:"Profile updated successfully!"});
+  } catch (err) {
+    console.error("Error during updation : ", err.message);
+    res.status(500).send("Server Error!");
+  }
 };
 
-const deleteUserProfile = (req, res) => {
-  res.send("DELETING USER PROFILE...");
+const deleteUserProfile = async (req, res) => {
+  const currentID = req.params.id;
+  try {
+    await connectClient();
+    const db = client.db("version-control-system");
+    const usersCollection = db.collection("users");
+
+    const result = await usersCollection.deleteOne({
+      _id: new ObjectId(currentID),
+    });
+
+    if(result.deleteCount == 0){
+      return res.status(404).json({message:"User Not Found!"});
+    }
+
+    res.json({message:"Users profile deleted!"}) ;
+  } catch (err) {
+    console.error("Error during deleting : ", err.message);
+    res.status(500).send("Server Error!");
+  }
 };
 
 module.exports = {
